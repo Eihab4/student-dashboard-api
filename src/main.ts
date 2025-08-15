d567af2816dbbbfaa7adca891db7582047a9e353
Eihab Muhammed <eihabMuhammed56@gmail.com>
2025-08-15 11:26:30 +0300

    feat: added frontend uri directly

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: process.env.FRONTEND_URI,
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
